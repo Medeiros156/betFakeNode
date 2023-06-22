35f0e63c39f2327dcb23e6a8f0bbf47fbef801f8
Pedro Medeiros <pedromedeiros156@hotmail.com>
2023-06-22 01:38:32 -0300

    port fix

--- app.ts
+++ app.ts
@@ -20,7 +20,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ error: 'Internal Server Error' });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
